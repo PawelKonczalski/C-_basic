6083e66b2f4dd0a8ecf05e39bc4b984331ce34af
PawelKonczalski <50076223+PawelKonczalski@users.noreply.github.com>
2020-01-28 17:50:05 +0100

    first commit

---
+++ test/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace test
+{
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine(args[0]);
+            Console.ReadLine();
+            Console.WriteLine(args[1]);
+        }
+    }
+}
